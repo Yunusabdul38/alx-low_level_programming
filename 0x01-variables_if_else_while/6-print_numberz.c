b21e9a12ec79452ca4e6dd74ae0a793edab2fc06
yunus abdulmajid <yunusabdulmajidyunus38@gmail.com>
2022-07-04 21:50:08 -0700

    variables

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,29 +3,21 @@
 #include <unistd.h>
 
 /**
- *  * main - Entry point
- *
- *   * Description: prints char below 10
- *
- *    * Return: Always 0 (success)
- *
+ *main - Entry point
+ *Description: Print base 16 numbers
+ *Return: Always 0 (success)
  **/
 
 int main(void)
 
 {
+	int a;
 
-		int a;
-
-
-
-			for (a = 0; a < 10; a++)
-			{
-				putchar(a + '0');
-			}
-
-				putchar('\n');
-
-					return (0);
+	for (a = 0; a < 10; a++)
+	{
+	putchar(a + '0');
+	}
+	putchar('\n');
+	return (0);
 
 }
